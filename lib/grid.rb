949af3cbd8e130b9929405179ff38c5fac990ab4
Diego Regules <diegoregules@gmail.com>
2015-07-29 13:17:33 +0100

    Grid updates

--- lib/grid.rb
+++ lib/grid.rb
@@ -3,7 +3,7 @@ require_relative 'cell'
 class Grid
 
   def bomb cell
+    fail 'Cell already bombed!' if cell.bombed?
     cell.bombed = true
   end
-
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -1,9 +1,15 @@
 require 'grid'
 
 describe Grid do
+  let(:unbombed_cell) { double('cell', :bombed? => false, :ship? => false)}
+  let(:bombed_cell) { double('cell', :bombed? => true, :ship? => false)}
 
   it 'responds to #bomb' do
     expect(subject).to respond_to(:bomb).with(1).argument
+  end
+
+  it 'does not allow to bomb same cell twice' do
+    expect { subject.bomb bombed_cell }.to raise_error 'Cell already bombed!'
   end
 
 end
